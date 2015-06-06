45cc06695c54f2bfcb1c0e219b2806177864393f
Louis Cheung <dev.louisc@gmail.com>
2015-06-06 11:58:28 -0400

    Fix space/tab inconsistencies by converting tabs to spaces

--- example/app.js
+++ example/app.js
@@ -1,26 +1,26 @@
 'use strict';
 
 var express = require('express'),
-	routes = require('./routes'),
-	swagger = require('..');
+    routes = require('./routes'),
+    swagger = require('..');
 
 var app = express();
 
 swagger.init(app, {
-	swaggerJsonPath: '/api.json',
-	swaggerUiPath: '/api',
-	info: {
-		title: 'Hello World',
-		version: '1.0.0'
-	},
-	apis: ['./routes.js']
+    swaggerJsonPath: '/api.json',
+    swaggerUiPath: '/api',
+    info: {
+        title: 'Hello World',
+        version: '1.0.0'
+    },
+    apis: ['./routes.js']
 });
 
 routes.setup(app);
 
 var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
